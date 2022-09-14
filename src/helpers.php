c5ba24b84d55d13d3c31bf8d52f819eb35b63a2a
xsaven <gms.sw.xsaven@gmail.com>
2022-09-14 10:54:02 +0300

    src/helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -1,2 +1,12 @@
 <?php
 
+if (! function_exists('class_in_file')) {
+    /**
+     * @param  string  $file
+     * @return string|null
+     */
+    function class_in_file(string $file): ?string
+    {
+        return (new \Bfg\Attributes\ClassGetter())->getClassFullNameFromFile($file);
+    }
+}
